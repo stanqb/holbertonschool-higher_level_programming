06889efb6d88df2690493c48f41911dc96350bf5
stan <9857@holbertonstudents.com>
2025-01-25 15:24:49 +0100

    Ajout de la fonction roman_to_int pour convertir un nombre romain en entier

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    total = 0
+    for i in range(len(roman_string)):
+        current_value = roman_dict[roman_string[i]]
+        if (i + 1 < len(roman_string) and
+                current_value < roman_dict[roman_string[i + 1]]):
+            total -= current_value
+        else:
+            total += current_value
+
+    return total
