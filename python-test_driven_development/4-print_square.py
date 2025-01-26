14777d025a893621ed6e2e2a0523e0fda5fa01fc
stan <9857@holbertonstudents.com>
2025-01-26 21:29:20 +0100

    Modification du fichier .txt pour l'exercice 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-
-"""
-function that prints a square with #
-"""
+"""Module with a function that prints out a square of '#'s"""
 
 
 def print_square(size):
-    """ Function that prints a square with #
+    """Print out a square with the char '#' of the size determined by the
+    'size' argument.
+
     Args:
-        size: size of the square to print
-    Return:
-        No return
-    Raise:
-        TypeError: If size is not an integer number
+        size (int): Positive integer representing the length of the square.
+
+    Raises:
+        TypeError: if size is not an integer.
+        ValueError: if size is less than zero.
     """
 
-    if not isinstance(size, int):
+    if type(size) is not int:
         raise TypeError("size must be an integer")
 
-    if size < 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
 
     for i in range(size):
