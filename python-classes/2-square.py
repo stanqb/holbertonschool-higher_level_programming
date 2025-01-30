7c20f0db69f933125f8b2406674e0ea84ca69e82
stan <9857@holbertonstudents.com>
2025-01-30 21:24:24 +0100

    Correction du code sur le fichier 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+Size defaults to 0. Raise errors on invalid inputs.
+"""
+
+
 class Square:
-    """
-    This class defines a square with a private size attribute.
-    The size attribute must be an integer and greater than or equal to 0.
-    """
-
+    """A class that defines a square by size"""
     def __init__(self, size=0):
-        """
-        Instantiates a square with an optional size.
-
-        Arguments:
-            size: Size of the square (must be an integer >= 0).
-
-        Raises a TypeError exception if size is not an integer.
-        Raises a ValueError exception if size is less than 0.
-        """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
