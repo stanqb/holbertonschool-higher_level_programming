3ead57242e67e62bd71229eee9faae1c0c5f8853
stan <9857@holbertonstudents.com>
2025-01-30 21:48:50 +0100

    Correction de la task 4 pour respecter les attentes de la consigne

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,26 +1,63 @@
 #!/usr/bin/python3
+"""The Square
+
+definition of the square
+
+"""
+
 
 class Square:
-    """Define a square by its size"""
+    """a 2d square
 
-    def __init__(self, size=0):
-        """Initialize the square with a size"""
-        self.size = size
+    methods for manipulating it
+
+    """
 
     @property
     def size(self):
-        """Get the value of the size"""
+        """int: length of square sides
+
+        The setter validates that the size is an integer and is 0 or greater
+
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the value of the size, with validation"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
+
+    def __init__(self, size=0):
+        """Creates a square of a given size
+
+        Size of the square is hidden
+
+        Args:
+            size (int): length of the sides
+
+        Raises:
+            TypeError: size is not an integer
+            ValueError: size is negative
+
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Return the area of the square"""
+        """Returns the size of square
+
+        Returns:
+            size squared for area
+
+        """
+
         return self.__size ** 2
