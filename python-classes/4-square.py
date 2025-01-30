8151991642938a958ddebe0b56c997ed767d99e6
stan <9857@holbertonstudents.com>
2025-01-30 22:50:46 +0100

    Correction task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-
-"""This module defines a class representing a square.
-
-Raises:
-    TypeError: If the size provided is not an integer.
-    ValueError: If the size provided is less than zero.
-
-Returns:
-    int: The area of the square.
-
-Attributes:
-    size (int): The size of the square's sides.
-"""
+"""commentaire principal"""
 
 
 class Square:
-    """A class to represent a square shape.
+    """commentaire de classe"""
 
-    Attributes:
-        size (int): The size of the square's sides.
-    """
-    __size = None
+    def __init__(self, size=0):
+        """commentaire d'instance"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calcul de l aire"""
+
+        return int(self.__size) ** 2
 
     @property
     def size(self):
+        """getter pour size"""
+
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+        """setter pour size"""
+
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
         else:
-            self.__size = value
-
-    def __init__(self, __size=0):
-        self.__size = __size
-
-    def area(self):
-        return self.__size * self.__sizeB
+            raise TypeError("size must be an integer")
