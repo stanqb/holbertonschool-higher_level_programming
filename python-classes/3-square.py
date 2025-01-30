33dbd76c7769056ddf20517a07cab7200c86cda6
stan <9857@holbertonstudents.com>
2025-01-30 21:15:25 +0100

    Tâche 3 : ajout de la méthode area pour calculer l'aire du carré

---
+++ python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class defines a square with a private size attribute.
+    The size attribute must be an integer and greater than or equal to 0.
+    """
+
+    def __init__(self, size=0):
+        """
+        Instantiates a square with an optional size.
+
+        Arguments:
+            size: Size of the square (must be an integer >= 0).
+
+        Raises a TypeError exception if size is not an integer.
+        Raises a ValueError exception if size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area.
+        """
+        return self.__size ** 2
