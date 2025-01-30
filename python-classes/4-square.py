655aad69630fad56242983a9f1d823886628d11c
stan <9857@holbertonstudents.com>
2025-01-30 22:00:11 +0100

    Correction de la task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,63 +1,28 @@
 #!/usr/bin/python3
-"""The Square
-
-definition of the square
-
-"""
+""" Class Square that defines a square """
 
 
 class Square:
-    """a 2d square
-
-    methods for manipulating it
-
-    """
+    """ Class Square """
+    def __init__(self, size=0):
+        """ Constructor """
+        self.size = size
 
     @property
     def size(self):
-        """int: length of square sides
-
-        The setter validates that the size is an integer and is 0 or greater
-
-        """
-
+        """ Getter """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """ Setter """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
-    def __init__(self, size=0):
-        """Creates a square of a given size
-
-        Size of the square is hidden
-
-        Args:
-            size (int): length of the sides
-
-        Raises:
-            TypeError: size is not an integer
-            ValueError: size is negative
-
-        """
-
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
     def area(self):
-        """Returns the size of square
-
-        Returns:
-            size squared for area
-
-        """
-
-        return self.__size ** 2
+        """ Calculatre square area """
+        return(self.__size * self.__size)
