e73bb49d7386d0b360ddbd5a01817c178cfe2c31
stan <9857@holbertonstudents.com>
2025-01-30 21:03:06 +0100

    Task 1 : ajout de la classe Square avec un attribut privé size

---
+++ python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""
+Module 1-square
+Defines a class Square that represents a square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
