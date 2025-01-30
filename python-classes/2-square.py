1f76534a0ed316f5a5cc856e1a1044bebb7ffd0a
stan <9857@holbertonstudents.com>
2025-01-30 21:34:02 +0100

    Correction task 2-square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-"""
-This is the "Square"  module.
+"""The Square
 
-This module provides a simple Square class with initialize size.
-Size defaults to 0. Raise errors on invalid inputs.
+definition of the square
+
 """
 
 
 class Square:
-    """A class that defines a square by size"""
+    """a 2d square
+
+    methods for manipulating it
+
+    """
     def __init__(self, size=0):
-        if type(size) != int:
+        """Creates a square of a given size
+
+        Size of the square is hidden
+
+        Args:
+            size (int): length of the sides
+
+        Raises:
+            TypeError: size is not an integer
+            ValueError: size is negative
+
+        """
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
