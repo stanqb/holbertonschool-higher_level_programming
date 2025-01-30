4e76c170b21dc040b58607f521bb1e0c0cfeff1e
stan <9857@holbertonstudents.com>
2025-01-30 21:37:59 +0100

    Correction de la task 3 pour respecter les attentes de la consigne

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python3
+"""The Square
+
+definition of the square
+
+"""
+
+
 class Square:
-    """
-    This class defines a square with a private size attribute.
-    The size attribute must be an integer and greater than or equal to 0.
+    """a 2d square
+
+    methods for manipulating it
+
     """
 
     def __init__(self, size=0):
-        """
-        Instantiates a square with an optional size.
+        """Creates a square of a given size
 
-        Arguments:
-            size: Size of the square (must be an integer >= 0).
+        Size of the square is hidden
 
-        Raises a TypeError exception if size is not an integer.
-        Raises a ValueError exception if size is less than 0.
+        Args:
+            size (int): length of the sides
+
+        Raises:
+            TypeError: size is not an integer
+            ValueError: size is negative
+
         """
-        if not isinstance(size, int):
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """Returns the size of square
+
+        Returns:
+            size squared for area
+
         """
-        Returns the current square area.
-        """
+
         return self.__size ** 2
