075a481b877b4059cfa54c4b057e03988f2720cc
stan-holberton <9857@holbertonstudents.com>
2025-02-06 19:32:15 +0100

    Modification du fichier 7-base pour la task 7, changement des tests robustent

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
+"""Module"""
 
-"""Defines a base geometry class BaseGeometry."""
 
-
-class BaseGeometry:
-    """Reprsent base geometry."""
+class BaseGeometry():
+    """class BaseGeometry"""
 
     def area(self):
-        """Not yet implemented."""
+        """area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if not isinstance(value, int):  # Correction ici
-            raise TypeError("{} must be an integer".format(name))
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
