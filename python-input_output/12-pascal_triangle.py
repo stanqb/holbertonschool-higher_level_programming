49d3ad0d7420767f2ff413e23d0199619661250f
stan-holberton <9857@holbertonstudents.com>
2025-02-12 21:57:03 +0100

    Correction de l'exercice Pascal's Triangle pour respecter les standards et valider tous les tests

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""A module that generates Pascal's triangle"""
+
 
 def pascal_triangle(n):
-    """
-    Generate Pascal's triangle of size n
+    """Returns a list of lists representing Pascal's triangle of size n.
+
+    Args:
+        n (int): The number of rows in the triangle.
+
+    Returns:
+        list: A list of lists containing the triangle.
     """
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = [[1]]  # Initialisation avec la première ligne
 
     for i in range(1, n):
-        row = [1]  # Start each row with 1
+        row = [1]  # Chaque ligne commence par 1
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # End each row with 1
+        row.append(1)  # Chaque ligne finit par 1
         triangle.append(row)
 
     return triangle
