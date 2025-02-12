58c919606e6c382c70e5a35d4ca8a3d89adbef90
stan-holberton <9857@holbertonstudents.com>
2025-02-12 20:53:52 +0100

    Ajout de la classe Student avec sérialisation et désérialisation

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of the instance.
+        If attrs is a list of strings, only attributes
+        matching the list are returned.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {key: getattr(self, key) for key in attrs
+                    if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Reloads the student's attributes from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
