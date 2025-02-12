6296a921e0bb30d9a769514d0a03f886f0cf4cbb
stan-holberton <9857@holbertonstudents.com>
2025-02-12 20:55:25 +0100

    Ajout du fichier 12-pascale_triangle pour afficher un TRIANGLE en python

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle of size n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]  # Start each row with 1
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)  # End each row with 1
+        triangle.append(row)
+
+    return triangle
