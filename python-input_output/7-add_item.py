9671b1a5f6e7d82a354cfa46212f370ad51bb1f9
stan-holberton <9857@holbertonstudents.com>
2025-02-14 00:43:16 +0100

    Amélioration de la gestion des fichiers JSON dans 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
+"""
+This script adds all arguments to a Python list,
+then saves them to a JSON file.
+"""
+
+import os
 import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
-# Charger la liste existante ou en créer une vide
-try:
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    my_list = []
+if __name__ == "__main__":
+    # Vérifier si fichier existe et charger son contenu, sinon créer une l vide
+    if os.path.isfile(filename):
+        try:
+            my_list = load_from_json_file(filename)
+        except ValueError:  # Gérer le cas où le fichier JSON est corrompu
+            my_list = []
+    else:
+        my_list = []
 
-# Ajouter les nouveaux arguments à la liste
-my_list.extend(sys.argv[1:])
+    # Ajouter les nouveaux arguments à la liste
+    my_list.extend(sys.argv[1:])
 
-# Sauvegarder la liste mise à jour dans le fichier
-save_to_json_file(my_list, filename)
+    # Sauvegarder la liste mise à jour dans le fichier
+    save_to_json_file(my_list, filename)
