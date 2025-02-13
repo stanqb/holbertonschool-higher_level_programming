80bad6c517638e087e2bf2c0ac9c031ed4159632
stan-holberton <9857@holbertonstudents.com>
2025-02-14 00:36:29 +0100

    Correction de la fonction pour charger un objet depuis un fichier JSON

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""Module for creating a Python object from a JSON file."""
+
 import json
 
 
 def load_from_json_file(filename):
-    """Crée un objet Python à partir d'un fichier JSON"""
-    with open(filename, 'r', encoding='utf-8') as file:
+    """Creates a Python object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to read.
+
+    Returns:
+        object: The Python object loaded from the file.
+    """
+    with open(filename, encoding="utf-8") as file:
         return json.load(file)
