92a003c86506c7527600c653012f3e0c2167b110
stan-holberton <9857@holbertonstudents.com>
2025-03-07 15:11:29 +0100

    Task 12 completed: Created script to list shows without any genre linked showing NULL values.

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Script that lists all shows in the database hbtn_0d_tvshows without a genre linked
+-- Each record displays: tv_shows.title - tv_show_genres.genre_id
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
+WHERE g.genre_id IS NULL
+ORDER BY s.title, g.genre_id ASC;
