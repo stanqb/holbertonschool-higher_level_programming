b929f12446e9ac2876a9e58136d43234f3881748
stan-holberton <9857@holbertonstudents.com>
2025-03-07 14:19:49 +0100

    Updated Task 1 with no option WITH GRANT OPTION.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Script that creates the MySQL server user user_0d_1 with all privileges
 -- and password user_0d_1_pwd. If the user already exists, the script will not fail.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
